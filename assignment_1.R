4f1ddd947ed032e440a59c28c4349f5773a0d050
Apple <apple@apples-air.mobile-devices.eur.nl>
2018-11-19 18:39:13 +0100

    Q2

--- assignment_1.R
+++ assignment_1.R
@@ -9,3 +9,10 @@ data$age<-parse_number(data$age)
 data$X...rank<-parse_number(data$X...rank)
 data$net_worth<-parse_number(data$net_worth)
 
+
+# Q2 ----------------------------------------------------------------------
+#In order to remove the people witj net_worth less than 1 bilion we remove the people
+# with rank higher than 1571
+data_new<-filter(data,data$X...rank <= 1571)
+
+
