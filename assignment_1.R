f9ce69de0ff7db1e4cb378323de536590e3d0cad
Apple <apple@apples-air.mobile-devices.eur.nl>
2018-11-19 18:54:58 +0100

    Q3

--- assignment_1.R
+++ assignment_1.R
@@ -9,10 +9,25 @@ data$age<-parse_number(data$age)
 data$X...rank<-parse_number(data$X...rank)
 data$net_worth<-parse_number(data$net_worth)
 
-
 # Q2 ----------------------------------------------------------------------
 #In order to remove the people witj net_worth less than 1 bilion we remove the people
 # with rank higher than 1571
 data_new<-filter(data,data$X...rank <= 1571)
+
+# Q3 ----------------------------------------------------------------------
+#First we plot age against net_worth
+ggplot(data_new)+
+  geom_point(aes(x=age,y=net_worth))
+#As we can see 39 missing values exist for age which are removes.
+
+#then we plot age against log(net_worth)
+ggplot(data_new)+
+  geom_point(aes(x=age,y=log(net_worth))) 
+#when we use log(net_worth) the plot is more informative since the observations for
+#lower values of net_worth (which contains the bigger portion of the data) is less dense
+#and easier to comprehend.
+
+
+
 
 
