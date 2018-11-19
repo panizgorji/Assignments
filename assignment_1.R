63510d03a087b5ad47ac391b2df9e7dae5e7e43b
Apple <apple@apples-air.mobile-devices.eur.nl>
2018-11-19 18:28:33 +0100

    part2-Q1

--- assignment_1.R
+++ assignment_1.R
@@ -3,3 +3,9 @@
 library(tidyverse)
 library(ggplot2)
 data<-read.csv("~/Documents/course/programming/forbes.csv")
+
+# Part2-Q1 ----------------------------------------------------------------
+data$age<-parse_number(data$age)
+data$X...rank<-parse_number(data$X...rank)
+data$net_worth<-parse_number(data$net_worth)
+
